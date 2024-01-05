3466625fc4966538939bc0fefe180f35163d0941
Maria Emanuilova <mariaemanuilova@yahoo.com>
2024-01-05 23:01:44 +0200

    Added event listener class and driver

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -3,25 +3,28 @@ package base;
 import com.google.common.io.Files;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import pages.HomePage;
+import utils.EventReporter;
 import utils.WindowManager;
 
 import java.io.File;
 import java.io.IOException;
 
 public class BaseTests {
-    protected WebDriver driver;
+    protected EventFiringWebDriver driver;
     protected HomePage homePage;
     protected String baseUrl = "https://the-internet.herokuapp.com/";
 
     @BeforeClass
     public void setUp() {
-        driver = new ChromeDriver();
+        driver = new EventFiringWebDriver(new ChromeDriver());
+        driver.register(new EventReporter());
         driver.get(baseUrl);
         driver.manage().window().maximize();
 
